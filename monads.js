c84a3330495dcd0c09bddc1feb0a46dba5ba982a
阮一峰 <53803693+ruanyifeng@users.noreply.github.com>
2019-08-05 22:30:51 -0400

    Update monads.js

--- monads.js
+++ monads.js
@@ -3,5 +3,23 @@ function first(f, [x, y]) {
 }
 
 function second(g, [x, y]) {
-  return [x, f(y)]
+  return [x, f(y)];
+}
+
+function bimap(f, g, [x, y]) {
+  return second(g, first(f, [x, y]));
+}
+
+class Functor {
+  
+}
+
+class Identity extends Functor {
+  constructor(x) {
+    this.runIdentity = x;
+  }
+  
+  fmap(f) {
+    return new Identity(f(this.runIdentity));
+  }
 }
